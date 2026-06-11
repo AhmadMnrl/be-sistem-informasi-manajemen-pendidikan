1e94f2da920e0bedadc70e4e0a2f2e5578b87894
AhmadMnrl <serigalaberbulu465@gmail.com>
2026-06-11 21:22:11 +0700

    fixing

--- src/utils/filePath.js
+++ src/utils/filePath.js
@@ -26,12 +26,12 @@ const BASE_URL = process.env.BASE_URL || "https://api.pospaudmelatiazzahra.com";
  */
 function buildImagePath(filename) {
   if (!filename) return null;
-  return `/uploads/images/${filename}`;
+  return `${BASE_URL}/uploads/images/${filename}`;
 }
 
 function buildDocumentPath(filename) {
   if (!filename) return null;
-  return `/uploads/documents/${filename}`;
+  return `${BASE_URL}/uploads/documents/${filename}`;
 }
 
 module.exports = { BASE_URL, normalizeFilePath, buildImagePath, buildDocumentPath };
